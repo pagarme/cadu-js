5632616fbe952b30473e0c49a15dd1eeb488badb
Marquinho Almeida <marco.almeida.junior@gmail.com>
2017-10-16 12:05:07 -0200

    client: create config
    - Create client validations
    - Create connect function

--- src/client.js
+++ src/client.js
@@ -1,27 +1,71 @@
-const forge = require('mappersmith').default
+const Promise = require('bluebird')
+const {
+  default: mappersmith,
+  configs,
+} = require('mappersmith')
 
-const moundHeader = () => {
-  'Content-type': 'application/json',
+const {
+  always,
+  equals,
+  ifElse,
+  isNil,
+  when,
+} = require('ramda')
+
+const memberRoutes = require('./routes/member')
+const contactRoutes = require('./routes/member/contact')
+const bankAccountRoutes = require('./routes/member/bank-account')
+const emailRoutes = require('./routes/member/email')
+const addressRoutes = require('./routes/member/address')
+const partnerRoutes = require('./routes/member/partner')
+const phoneRoutes = require('./routes/member/phone')
+const countryRoutes = require('./routes/others/country')
+const economicActivitiesRoutes = require('./routes/others/economic-activities')
+const analysisRoutes = require('./routes/risk/analysis')
+
+configs.Promise = Promise
+
+const connect = (options = {}) => {
+  const { token, env } = options
+
+  if (isNil(token)) {
+    throw new Error('Token is not defined.')
+  }
+
+  if (isNil(env)) {
+    throw new Error('Environment is not defined.')
+  }
+
+  const headers = {
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  }
+
+  const connectRoute = route => route(headers)
+
+  const chooseHost = ifElse(
+    equals('live'),
+    always('https://api-cadu.stone.com.br'),
+    always('https://api-sandbox-cadu.stone.com.br')
+  )
+
+  const library = mappersmith({
+    host: chooseHost(env),
+    resources: {
+      Member: connectRoute(memberRoutes),
+      Contact: connectRoute(contactRoutes),
+      BankAccount: connectRoute(bankAccountRoutes),
+      Email: connectRoute(emailRoutes),
+      Address: connectRoute(addressRoutes),
+      Partner: connectRoute(partnerRoutes),
+      Phone: connectRoute(phoneRoutes),
+      Country: connectRoute(countryRoutes),
+      EconomicActivity: connectRoute(economicActivitiesRoutes),
+      Analysis: connectRoute(analysisRoutes),
+    },
+  })
+
+  return library
 }
 
-const defineHost () => 'https://api-sandbox-cadu.stone.com.br/'
-
-
-const client = forge({
-  host: defineHost(),
-  resource: {
-    Member: {
-      all: {
-        path: '/membership'
-      },
-      byId: {
-        path: '/membership/{memberKey}'
-      },
-      create: {
-        method: 'post',
-        path: '/membership',
-        headers: mountHeader()
-      }
-    }
-  }
-})
+module.exports = { connect }
