89124f81f7edda53e95bb71357e7c6c83a8b2493
Marquinho Almeida <marco.almeida.junior@gmail.com>
2017-10-17 02:39:25 -0200

    task: fix test client.js

--- src/client.test.js
+++ src/client.test.js
@@ -1,11 +1,18 @@
 const cadujs = require('./client')
 
-test('create client without token', () => {
-  expect(() => cadujs.connect({ env: 'sandbox' }))
-    .toThrow('Token is not defined.')
-})
+describe('Create client', () => {
+  test('without token', () => {
+    expect(() => cadujs.connect({ env: 'sandbox' }))
+      .toThrow('Token is not defined.')
+  })
 
-test('create client with token and without env', () => {
-  expect(() => cadujs.connect({ token: '1234' }))
-    .toThrow('Environment is not defined.')
+  test('with token and without env', () => {
+    expect(() => cadujs.connect({ token: '1234' }))
+      .toThrow('Environment is not defined.')
+  })
+
+  test('with token and with env', () => {
+    expect(() => cadujs.connect({ token: '1234', env: 'sandbox' }))
+      .toBeDefined()
+  })
 })
