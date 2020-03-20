226ead028189f1f6e847d0994280696fcd8c3542
Eduardo Pereira <eduardo.soares@pagar.me>
2020-03-20 17:47:10 -0300

    adapters: trim address properties

--- src/adapters/pagarme/address.js
+++ src/adapters/pagarme/address.js
@@ -5,25 +5,31 @@ const {
   pipe,
   prop,
   slice,
+  trim,
   when,
 } = require('ramda')
 
 const isString = is(String)
 
+const propAndTrim = propName => pipe(
+  prop(propName),
+  when(isString, trim)
+)
+
 const complement = pipe(
-  prop('complementary'),
+  propAndTrim('complementary'),
   when(isString, slice(0, 63))
 )
 
 const adapter = applySpec({
   typeId: always(2),
-  streetName: prop('street'),
-  entranceNumber: prop('street_number'),
-  neighborhood: prop('neighborhood'),
-  postalCode: prop('zipcode'),
+  streetName: propAndTrim('street'),
+  entranceNumber: propAndTrim('street_number'),
+  neighborhood: propAndTrim('neighborhood'),
+  postalCode: propAndTrim('zipcode'),
   complement,
-  cityName: prop('city'),
-  countrySubdivisionCode: prop('state'),
+  cityName: propAndTrim('city'),
+  countrySubdivisionCode: propAndTrim('state'),
   countryId: always(76),
 })
 
